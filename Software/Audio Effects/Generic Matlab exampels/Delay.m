b14670be915a868e9b9aed6213f1aaec4d420460
Christoffer Forsberg <cihfor-4@student.ltu.se>
2019-02-12 21:41:14 +0100

    added a basic delay example in matlab

---
+++ Software/Audio Effects/Generic Matlab exampels/Delay.m
@@ -0,0 +1,30 @@
+close all;clear all;clc
+%read in song
+load gong.mat;
+
+fDelayInPercent = 0.2; 
+iSamplingsFrequency = 8192; %Sampling frequency
+fOriginalSoundHardness = 0.5;
+fImpulseHardness = 0.5;
+iTotalNumberOfSampels=length(y); %Length of input signal. 
+aSoundVector=y(:,1); %Vector form/2D version of length 
+iDelayInSamples = round(fDelayInPercent*iTotalNumberOfSampels); %Delay 
+ 
+iDelayedSignalSampels = length(y); %Length of the delayed signal. 
+aImpulseVector = zeros(length(y),1); %Impulse vector
+iImpulseVector(1) = fOriginalSoundHardness; 
+iImpulseVector(iDelayInSamples)= fImpulseHardness ; %Impulse at delay 
+aEchoedSound = conv(aSoundVector,iImpulseVector); %Convolution
+
+
+%plots
+subplot(3,1,1);
+plot(y); %Plot of original voice 
+subplot(3,1,2);
+plot(iImpulseVector); %plot of impulse  
+subplot(3,1,3); 
+plot(aEchoedSound); %Plot of Echoed sound 
+ 
+sound(aEchoedSound,iSamplingsFrequency); %Echoed sound
+
+
