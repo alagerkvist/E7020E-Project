628b7e68455b62662f801898e543c8ffe7a6c4c1
Christoffer Forsberg <cihfor-4@student.ltu.se>
2019-02-18 11:51:38 +0100

    updated Generic Matlab code to show legends on plots

--- Software/Audio Effects/Generic Matlab exampels/Delay.m
+++ Software/Audio Effects/Generic Matlab exampels/Delay.m
@@ -4,27 +4,38 @@ load gong.mat;
 
 fDelayInPercent = 0.5; 
 iSamplingsFrequency = 8192; %Sampling frequency
-fOriginalSoundHardness = 0.1;
+fOriginalSoundHardness = 0;
 fImpulseHardness = 0.5;
 iTotalNumberOfSampels=length(y); %Length of input signal. 
 aSoundVector=y(:,1); %Vector form/2D version of length 
 iDelayInSamples = round(fDelayInPercent*iTotalNumberOfSampels); %Delay 
- 
-iDelayedSignalSampels = length(y); %Length of the delayed signal. 
+  
 aImpulseVector = zeros(length(y),1); %Impulse vector
-iImpulseVector(1) = fOriginalSoundHardness; 
-iImpulseVector(iDelayInSamples)= fImpulseHardness ; %Impulse at delay 
-aDelayedSound = conv(aSoundVector,iImpulseVector); %Convolution
+aImpulseVector(1) = fOriginalSoundHardness; 
+aImpulseVector(iDelayInSamples)= fImpulseHardness ; %Impulse at delay 
+aDelayedSound = conv(aSoundVector,aImpulseVector); %Convolution
 
 
 %plots
 subplot(3,1,1);
 plot(y); %Plot of original voice 
+title('Original sound');
+ylabel('Amplitude');
+xlabel('Sampel');
+
 subplot(3,1,2);
-plot(iImpulseVector); %plot of impulse  
+plot(aImpulseVector); %plot of impulse  
+title('Impulse answer');
+ylabel('Amplitude');
+xlabel('Sampel');
+
 subplot(3,1,3); 
 plot(aDelayedSound); %Plot of Echoed sound 
- 
+title('Output sound');
+ylabel('Amplitude');
+xlabel('Sampel');
+
+
 sound(aDelayedSound,iSamplingsFrequency); %Echoed sound
 
 
