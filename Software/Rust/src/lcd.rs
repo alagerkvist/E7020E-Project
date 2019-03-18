a1af71422520e0c01f9f170b6a95b19a2440eea5
alagerkvist <lager.kvist@hotmail.com>
2019-03-18 16:24:18 +0100

    added lcd to main, not fully working

---
+++ Software/Rust/src/lcd.rs
@@ -0,0 +1,170 @@
+#![no_main]
+#![no_std]
+
+extern crate stm32f4xx_hal as hal;
+use hal::prelude::_embedded_hal_digital_OutputPin as OutputPin;
+use hal::prelude::_embedded_hal_blocking_spi_Write as Write;
+use hal::spi::{Spi, Mode, Phase, Polarity, NoMiso};
+
+
+#[derive(Debug)]
+pub enum Error<E> {
+    Spi(E),
+}
+
+enum Command{
+
+}
+
+enum Letter{
+    Space,
+    Percent,
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Colon,
+    D,
+    c,
+    e,
+    l,
+    t,
+    u,
+    y,
+}
+
+impl Letter {
+    fn value(&self) -> &[u8] {
+        match *self {
+            Letter::Space => &[0,0,0,0,0,0],
+            Letter::Percent => &[35, 19, 8, 100, 98, 0],
+            Letter::Zero => &[62, 81, 73, 69, 62,  0],
+            Letter::One => &[0, 66,127, 64,  0,  0],
+            Letter::Two => &[66, 97, 81, 73, 70,  0],
+            Letter::Three => &[33, 65, 73, 77, 51,  0],
+            Letter::Four => &[24, 20, 18,127, 16,  0],
+            Letter::Five => &[39, 69, 69, 69, 57,  0],
+            Letter::Six => &[60, 74, 73, 73, 48,  0],
+            Letter::Seven => &[65, 33, 17,  9,  7,  0],
+            Letter::Eight => &[54, 73, 73, 73, 54,  0],
+            Letter::Nine => &[6, 73, 73, 41, 30,  0],
+            Letter::Colon => &[0,  0, 40,  0,  0,  0],
+            Letter::D => &[127, 65, 65, 65, 62,  0],
+            Letter::c => &[56, 68, 68, 68, 40,  0],
+            Letter::e => &[56, 84, 84, 84, 24,  0],
+            Letter::l => &[0, 65,127, 64,  0,  0,],
+            Letter::t => &[4, 4, 63, 68, 36, 0],
+            Letter::u => &[60, 64, 64, 32, 124, 0],
+            Letter::y => &[76, 144, 144, 144, 124, 0],
+        }
+    }
+}
+/*
+pub struct EA_dogs102_6w<SPI, CS, CD, RESET> {
+    spi: SPI,
+    cs: CS,
+    cd: CD,
+    reset: RESET,
+}
+*/
+pub struct EA_dogs102_6w<SPI, CS, CD> {
+    spi: SPI,
+    cs: CS,
+    cd: CD,
+}
+
+
+impl<E, SPI, CS, CD> EA_dogs102_6w<SPI, CS, CD>
+where
+    SPI: Write<u8, Error = E>,
+    CS: OutputPin,
+    CD: OutputPin,
+{
+    pub fn init(
+        spi: SPI,
+        cs: CS,
+        cd: CD,
+    ) -> Result<Self, Error<E>> {
+        let mut ea_dogs = EA_dogs102_6w {
+            spi,
+            cs,
+            cd,
+        };
+        ea_dogs.cd.set_low();
+        ea_dogs.cs.set_high();
+        ea_dogs.cs.set_low();
+        ea_dogs.spi.write(&[0x40, 0xA1, 0xC0, 0xA4, 0xA6, 0xA2, 0x2F, 0x27, 0x81, 0x10, 0xFA, 0x90, 0xAF]);
+        ea_dogs.cs.set_high();
+        ea_dogs.clear_screen();
+        Ok(ea_dogs)
+    }
+
+    pub fn update_duty(&mut self, duty: u32){
+        self.cd.set_low();
+        self.cs.set_low();
+        let msb_adress = 0x10 + (4>>4);
+        let lsb_adress = 0x00 + (2&0x0F);
+        let adress_page = 0xB0 + (0&0x0F);
+        self.spi.write(&[msb_adress, lsb_adress, adress_page]);
+        
+        self.cd.set_high();
+        self.spi.write(Letter::Zero.value());
+    }
+
+    pub fn write_word(&mut self, s: &str){
+        self.cd.set_low();
+        self.cs.set_low();
+        let msb_adress = 0x10 + (0x42>>4);
+        let lsb_adress = 0x00 + (0x42&0x0F);
+        let adress_page = 0xB0 + (0&0x0F);
+        self.spi.write(&[msb_adress, lsb_adress, adress_page]);
+        self.cd.set_high();
+        for (_,letter) in s.chars().enumerate(){
+            match letter {
+                ' ' => { self.spi.write(Letter::Space.value()); },
+                ':' => { self.spi.write(Letter::Colon.value()); },
+                '0' => { self.spi.write(Letter::Zero.value()); },
+                '1' => { self.spi.write(Letter::One.value()); },
+                '2' => { self.spi.write(Letter::Two.value()); },
+                '3' => { self.spi.write(Letter::Three.value()); },
+                '4' => { self.spi.write(Letter::Four.value()); },
+                '5' => { self.spi.write(Letter::Five.value()); },
+                '6' => { self.spi.write(Letter::Six.value()); },
+                '7' => { self.spi.write(Letter::Seven.value()); },
+                '8' => { self.spi.write(Letter::Eight.value()); },
+                '9' => { self.spi.write(Letter::Nine.value()); },
+                'D' => { self.spi.write(Letter::D.value()); },
+                'u' => { self.spi.write(Letter::u.value()); },
+                't' => { self.spi.write(Letter::t.value()); },
+                'y' => { self.spi.write(Letter::y.value()); },
+                'c' => { self.spi.write(Letter::c.value()); },
+                'l' => { self.spi.write(Letter::l.value()); },
+                'e' => { self.spi.write(Letter::e.value()); },
+                _  => {},
+            };
+        }
+        self.cs.set_high();
+    }
+
+    pub fn clear_screen(&mut self){
+        for page in 0..8 {
+            //asm::bkpt();
+            self.cs.set_low();
+            self.cd.set_low();
+            let msb_adress = 0x10 + (0>>4);
+            let lsb_adress = 0x00 + (0&0x0F);
+            let adress_page = 0xB0 + (page&0x0F);
+            self.spi.write(&[msb_adress, lsb_adress, adress_page]);
+            self.cd.set_high();
+            for _ in 0..102 {
+                self.spi.write(&[0x00]);
+            } 
+        }
+    }
+}
--- Software/Rust/src/main.rs
+++ Software/Rust/src/main.rs
@@ -7,16 +7,18 @@ extern crate cortex_m;
 extern crate panic_halt;
 //extern crate stm32f4;
 extern crate stm32f4xx_hal as hal;
+use crate::hal::prelude::*;
 use cortex_m::{asm, iprintln};
 use hal::stm32::ITM;
 use hal::stm32::ADC1;
-use stm32f4::stm32f413::GPIOB;
+use hal::stm32::GPIOB;
+use hal::spi::{Spi, Mode, Phase, Polarity, NoMiso};
 use rtfm::{app, Instant};
+mod lcd;
+use lcd::*;
+const PERIOD: u32 = 400_000;
 
 #[app(device = hal::stm32)]
-const PERIOD: u32 = 4_000_000;
-
-
 const APP: () = {
     static mut GPIOB: GPIOB = ();
     static mut Duty_cycle: f32 = 0.5; // Duty range = [20 80]%
@@ -24,15 +26,18 @@ const APP: () = {
     static mut Pwm_off: u32 = 0;
     static mut ITM: ITM = ();    
     static mut ADC: ADC1 = ();
-
+    static mut LCD: lcd::EA_dogs102_6w = None;
     //static mut adc_data: dr<hal::stm32::adc1> = ();
     // init runs in an interrupt free section
-    #[init(schedule = [on], resources=[Pwm_on, Pwm_off, Duty_cycle])]
+    #[init(schedule = [on], resources=[Pwm_on, Pwm_off, Duty_cycle], spawn=[update_pwm])]
     fn init() {
         //let mut c = hal::stm32::CorePeripherals::take().unwrap();
         let stim = &mut core.ITM.stim[0];
+       
         device.RCC.ahb1enr.modify(|_, w| w.gpioaen().set_bit()); //Enable GPIOA clock
         device.RCC.apb2enr.modify(|_, w| w.adc1en().set_bit());
+        device.RCC.ahb1enr.modify(|_, w| w.gpioben().set_bit());
+
         device.ADC1.cr2.modify(|_, w| w.adon().clear_bit());
         device.ADC1.cr1.modify(|_, w| w.eocie().set_bit());
         device.ADC1.cr2.modify(|_, w|{
@@ -46,24 +51,55 @@ const APP: () = {
         device.ADC1.smpr2.modify(|_, w| w.smpx_x().cycles480());
         device.ADC1.cr2.modify(|_, w| w.adon().set_bit());
         device.ADC1.sqr3.modify(|_, w| unsafe { w.sq1().bits(0 as u8) });
-        device.GPIOA.afrl.modify(|_, w| w.afrl0().bits(0));
-        device.GPIOA.moder.modify(|_, w| w.moder0().bits(0b11)); //Analog mode
-        device.GPIOA.pupdr.modify(|_, w| unsafe { w.pupdr0().bits(0b00) });
+        //device.ADC1.sqr3.modify(|_, w| unsafe { w.sq2().bits(2 as u8) });
+        
+        device.ADC1.sqr1.modify(|_,w| w.l().bits(0));
+        let gpioa = device.GPIOA.split();
+        gpioa.pa0.into_analog();
+        //gpioa.pa2.into_analog();
+        
+        let rcc = device.RCC.constrain();
+
+        // 16 MHz (default, all clocks)
+        let clocks = rcc.cfgr.freeze();
+        let gpioc = device.GPIOC.split();
+        let sck = gpioc.pc10.into_alternate_af6();
+        let mosi = gpioc.pc12.into_alternate_af6();
+        
+        let mut cd = gpioc.pc11.into_push_pull_output();
+        let mut cs = gpioa.pa15.into_push_pull_output();
+
+        pub const MODE: Mode = Mode {
+            polarity: Polarity::IdleHigh,
+            phase: Phase::CaptureOnFirstTransition,
+        };
+        let mut spi = Spi::spi3(
+            device.SPI3,
+            (sck, NoMiso, mosi),
+            MODE,
+            10_000_000.hz(),
+            clocks
+        );
+
+        let mut lcd1 = lcd::EA_dogs102_6w::init(spi, cs, cd).unwrap();
+        lcd1.write_word("Duty cycle: ");
+        
+        //lcd1.init();
+        
         iprintln!(stim, "main");
         //device.ADC1.cr2.modify(|_, w| w);
         device.ADC1.cr2.modify(|_, w| w.swstart().set_bit());
-        *resources.Pwm_on = (PERIOD as f32 * *resources.Duty_cycle) as u32;
-        *resources.Pwm_off = PERIOD - *resources.Pwm_on;
+        //*resources.Pwm_on = (PERIOD as f32 * *resources.Duty_cycle) as u32;
+        //*resources.Pwm_off = PERIOD - *resources.Pwm_on;
+        spawn.update_pwm(*resources.Duty_cycle);
         //schedule.foo(Instant::now() + PERIOD.cycles()).unwrap();
-    
-        // power on GPIOA, RM0368 6.3.11
-        device.RCC.ahb1enr.modify(|_, w| w.gpioben().set_bit());
         // configure PA5 as output, RM0368 8.4.1
         device.GPIOB.moder.modify(|_, w| w.moder1().bits(1));
 
         // pass on late resources
 
         schedule.on(Instant::now());
+        LCD = lcd1;
         GPIOB = device.GPIOB;
         ADC = device.ADC1;
         ITM = core.ITM;
@@ -90,19 +126,45 @@ const APP: () = {
         schedule.on(scheduled + a.cycles()).unwrap();
     }
 
-    #[task(priority = 1, resources = [ITM])]
+    #[task(priority = 3, resources = [Pwm_on, Pwm_off, Duty_cycle])]
+    fn update_pwm(dc: f32){
+        let mut pwm: u32 = 0;
+        resources.Pwm_on.lock(|pwm_on|{
+            *pwm_on = (PERIOD as f32 * dc) as u32;
+            pwm = *pwm_on;
+        });
+        resources.Pwm_off.lock(|pwm_off|{
+            *pwm_off = PERIOD - pwm;
+        });
+        *resources.Duty_cycle = dc;
+    }
+
+    #[task(priority = 3, resources = [ITM, Duty_cycle], spawn = [update_pwm])]
     fn change_freq(val: u32) {
         let duty_cycle_percent: f32 = ((val as f32) * (80.0-20.0) / (4050.0) + 20.0);
+        spawn.update_pwm(duty_cycle_percent / 100.0);
         let stim = &mut resources.ITM.stim[0];
+
         iprintln!(stim, "val: {}", duty_cycle_percent);
     }
 
-    #[interrupt(priority = 2, resources = [ADC], spawn = [change_freq])]
+    #[task(priority = 1, resources = [ADC])]
+    fn restart_adc(){
+        resources.ADC.lock(|adc|{
+            adc.sr.modify(|_, w| w.eoc().clear_bit());
+            adc.sr.modify(|_, w| w.ovr().clear_bit());
+            adc.cr2.modify(|_, w| w.swstart().set_bit()); 
+        });
+    }
+
+    #[interrupt(priority = 3, resources = [ADC], spawn = [change_freq,restart_adc], schedule=[restart_adc])]
     fn ADC() {
         let value: u32 = resources.ADC.dr.read().bits();
         spawn.change_freq(value);
-        resources.ADC.sr.modify(|_, w| w.eoc().clear_bit());
-        resources.ADC.cr2.modify(|_, w| w.swstart().set_bit());         
+        //resources.ADC.cr2.modify(|_, w| w.swstart().set_bit());         
+        spawn.restart_adc();
+        //schedule.restart_adc(Instant::now() + 4_000_000.cycles()).unwrap();
+
     }
 
     extern "C" {
